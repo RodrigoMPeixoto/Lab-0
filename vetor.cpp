77f1cb46b466dc710ae98550ee33cbbe23241bd1
Rodrigo Peixoto <peixoto.rodrigo@ufba.br>
2020-10-02 19:49:15 +0000

    Não entendi como fazer a terceira função.

--- vetor.cpp
+++ vetor.cpp
@@ -3,15 +3,18 @@
 #include <cstdlib>
 #include <limits>
 #include <algorithm>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
 
-#define MAX 200
+#define MAX 10
 
 // ********************************************************************
 // ** Funcao que determina o maior elemento de um vetor com N elementos
 // ********************************************************************
 int maxVetor(std::vector<int> v) {
 
-int max = v[0];
+	int max = v[0];
 
     for (unsigned int i = 1 ; i < v.size() ; i++)
         if (max < v[i])
@@ -26,8 +29,36 @@ int max = v[0];
 // ** Primeira versão.
 // ********************************************************************
 int minMaxVetor1(std::vector<int> v) {
+  int min, max, count=0;
+  min = v[0];
+  max = v[0];
+  
+  for(unsigned int i=1; i<v.size();i++)
+  {
+    if(min > v[i])
+    {
+      min = v[i];
+      count++;
+    }else
+	{
+		count++;
+	}
+    
+    if(max < v[i])
+    {
+      max = v[i];
+      count++;
+    }else
+	{
+		count++;
+	}
+  }
+  
+  std::cout << "Contagem minMaxCetor1: " << count << std::endl;
+  std::cout << "Max: " << max << std::endl;
+  std::cout << "Min: " << min << std::endl;
 
-    return 0;
+  return 0;
 }
 
 // ********************************************************************
@@ -36,8 +67,32 @@ int minMaxVetor1(std::vector<int> v) {
 // ** Segunda versão.
 // ********************************************************************
 int minMaxVetor2(std::vector<int> v) {
+  int min, max, count=0;
+  min = v[0];
+  max = v[0];
 
-    return 0;
+  for(unsigned int i=1; i<v.size();i++)
+  {
+  	
+    if(min > v[i])
+    {
+      min = v[i];
+      count++;
+    }else if(max < v[i])
+    {
+      max = v[i];
+      count++;
+    }else
+	{
+		count++;
+	}
+  }
+  
+  std::cout << "Contagem minMaxCetor2: " << count << std::endl;
+  std::cout << "Max: " << max << std::endl;
+  std::cout << "Min: " << min << std::endl;
+
+  return 0;
 }
 
 // ********************************************************************
@@ -46,8 +101,26 @@ int minMaxVetor2(std::vector<int> v) {
 // ** Terceira versão.
 // ********************************************************************
 int minMaxVetor3(std::vector<int> v) {
+  int min, max,count =0;
+  min = v[0];
+  max = v[0];
+  //Não entendi como fazer o terceiro formato
+  for(unsigned int i=1; i<v.size();i++)
+  {
+    if(min > v[i])
+    {
+      min = v[i];
+    }else if(max < v[i])
+    {
+      max = v[i];
+    }
+  }
+  
+  std::cout << "Contagem minMaxCetor3: " << count << std::endl;
+  std::cout << "Max: " << max << std::endl;
+  std::cout << "Min: " << min << std::endl;
 
-    return 0;
+  return 0;
 }
 
 // ********************************************************************
@@ -55,11 +128,11 @@ int minMaxVetor3(std::vector<int> v) {
 // ********************************************************************
 int main() {
 
-std::vector<int> v;
+	std::vector<int> v;
 
 	std::cout << "tamanho do vetor = " << v.size() << std::endl;
 
-    srand ( time(NULL) );
+    srand(time(NULL));
 
     for (unsigned int i=0; i < MAX; i++) {
         v.push_back(rand() % std::numeric_limits<int>::max());
@@ -68,7 +141,10 @@ std::vector<int> v;
     std::cout << "conteudo de v: ";
     for (std::vector<int>::iterator it = v.begin() ; it != v.end(); ++it)
        std::cout << ' ' << *it << std::endl;
-
+   
+	minMaxVetor1(v);
+	minMaxVetor2(v);
+	minMaxVetor3(v);
     std::cout << "====> O maior elemento do conjunto e': " << maxVetor(v) << std::endl;
     	
  	return 0;        
